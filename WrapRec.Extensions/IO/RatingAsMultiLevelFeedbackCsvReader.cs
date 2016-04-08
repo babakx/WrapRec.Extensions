e3dc33b15f1e0147e675d4ea4fdb9133f3d5c914
Babak Loni <babak.loni@gmail.com>
2016-04-08 16:36:10 +0200

    Added RatingAsMultiLevelFeedback CsvReader, also updated the sampling in ML-BPR

---
+++ WrapRec.Extensions/IO/RatingAsMultiLevelFeedbackCsvReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WrapRec.Core;
+using WrapRec.IO;
+
+namespace WrapRec.Extensions.IO
+{
+	public class RatingAsMultiLevelFeedbackCsvReader : CsvReader
+	{
+		public int MaxRating { get; set; }
+
+		public override void Setup()
+		{
+			base.Setup();
+			MaxRating = int.Parse(SetupParameters["maxRating"]);
+		}
+		
+		protected override void EnrichFeedback(Feedback feedback)
+		{
+			feedback.Level = MaxRating - (int) ((Rating)feedback).Value;
+		}
+	}
+}
--- WrapRec.Extensions/Models/MultiLevelBPRFM.cs
+++ WrapRec.Extensions/Models/MultiLevelBPRFM.cs
@@ -12,23 +12,89 @@ namespace WrapRec.Extensions.Models
 		int[] _feedbackLevels;
 		int _numUnobservedSamples = 0;
 		int _numObservedSamples = 0;
+		Dictionary<string, List<Feedback>> _trainUserPosFeedback;
+		Dictionary<string, List<Feedback>> _trainUserNegFeedback;
+		Dictionary<string, List<string>> _trainUserPosItems;
+		List<Feedback> _trainFeedback;
+
+		int _numTrainSamples;
+
+		public bool IsRatingBased { get; set; }
+		public bool OverSamplePopular { get; set; }
 
 		protected override void InitModel()
 		{
 			base.InitModel();
+			Split.UpdateFeedbackSlices();
 			_feedbackLevels = Split.Train.Select(f => f.Level).Distinct().ToArray();
+			
+			_trainUserPosFeedback = new Dictionary<string, List<Feedback>>();
+			_trainUserNegFeedback = new Dictionary<string, List<Core.Feedback>>();
+			_trainUserPosItems = new Dictionary<string, List<string>>();
+			_trainFeedback = Split.Train.ToList();
+			_numTrainSamples = _trainFeedback.Count;
+
+			var usersFeedback = Split.Train.GroupBy(f => f.User);
+
+			if (IsRatingBased)
+			{
+				foreach (var g in usersFeedback)
+				{
+					float ratingAvg = g.Average(f => ((Rating)f).Value);
+					_trainUserPosFeedback.Add(g.Key.Id, g.Where(f => ((Rating)f).Value > ratingAvg).ToList());
+					_trainUserNegFeedback.Add(g.Key.Id, g.Where(f => ((Rating)f).Value <= ratingAvg).ToList());
+					_trainUserPosItems.Add(g.Key.Id, g.Where(f => ((Rating)f).Value > ratingAvg).Select(f => f.Item.Id).ToList());
+				}
+			}
+			else
+			{
+				_trainUserPosFeedback = usersFeedback.ToDictionary(g => g.Key.Id, g => g.ToList());
+				_trainUserNegFeedback = usersFeedback.ToDictionary(g => g.Key.Id, g => new List<Feedback>());
+			}
+		}
+
+		public override void Iterate()
+		{
+			if (!OverSamplePopular)
+			{
+				base.Iterate();
+				return;
+			}
+
+			for (int i = 0; i < Feedback.Count; i++)
+			{
+				int user_id = SampleUser();
+				string userIdOrg = UsersMap.ToOriginalID(user_id);
+				var userPosFeedback = _trainUserPosFeedback[userIdOrg];
+				var userPosItems = _trainUserPosItems[userIdOrg];
+
+				var samplePosFeedback = userPosFeedback[random.Next(userPosFeedback.Count)];
+				int item_id = ItemsMap.ToInternalID(samplePosFeedback.Item.Id);
+
+				string negItemIdOrg; 
+				do
+				{
+					negItemIdOrg = _trainFeedback[random.Next(_numTrainSamples)].Item.Id;
+				} while (userPosItems.Contains(negItemIdOrg));
+
+				int negItemId = ItemsMap.ToInternalID(negItemIdOrg);
+
+				UpdateFactors(user_id, item_id, negItemId, true, true, update_j);
+			}
 		}
 
 		protected override void SampleTriple(out int user_id, out int item_id, out int other_item_id)
 		{
 			user_id = SampleUser();
 			string userIdOrg = UsersMap.ToOriginalID(user_id);
-			var userFeedback = Split.Container.Users[userIdOrg].Feedbacks.Where(f => f.SliceType == FeedbackSlice.TRAIN).ToList();
+			var userPosFeedback = _trainUserPosFeedback[userIdOrg];
 
-			var sampleFeedback = userFeedback[random.Next(userFeedback.Count)];
+			var sampleFeedback = userPosFeedback[random.Next(userPosFeedback.Count)];
 			item_id = ItemsMap.ToInternalID(sampleFeedback.Item.Id);
 
-			var toSampleLevels = userFeedback.Select(f => f.Level).Distinct().Where(l => l > sampleFeedback.Level).ToList();
+			var allUserFeedback = userPosFeedback.Concat(_trainUserNegFeedback[userIdOrg]).ToList();
+
+			var toSampleLevels = allUserFeedback.Select(f => f.Level).Distinct().Where(l => l > sampleFeedback.Level).ToList();
 			int sampleLevel = random.Next(toSampleLevels.Count + 1);
 
 			// if there is no lower level feedbacks, sample from unobserved
@@ -39,7 +105,7 @@ namespace WrapRec.Extensions.Models
 			}
 			else
 			{
-				var toSample = userFeedback.Where(f => f.Level == toSampleLevels[sampleLevel]).ToList();
+				var toSample = allUserFeedback.Where(f => f.Level == toSampleLevels[sampleLevel]).ToList();
 				other_item_id = ItemsMap.ToInternalID(toSample[random.Next(toSample.Count)].Item.Id);
 				_numObservedSamples++;
 			}
